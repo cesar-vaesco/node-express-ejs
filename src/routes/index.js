1242287fcec768956e77dde4a22b6f8593ccb47d
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-07-01 14:16:24 -0500

    Pasando los datos guardados a la vista

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,13 @@ const { Router } = require("express");
 
 const router = Router();
 
+const books = [];
+
 
 router.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs',{
+        books
+    });
 });
 
 router.get('/new-entry', (req, res) => {
@@ -12,7 +16,23 @@ router.get('/new-entry', (req, res) => {
 });
 
 router.post('/new-entry', (req, res) => {
-    console.log(req.body);
+    /* console.log(req.body); */
+    // guardarm en el arreglo los datos que llegan del request
+    const { title, author, image, description } = req.body;
+
+    if (!title || !author || !image || !description) {
+        res.status(400).send('La información enviada es incompleta');
+    }
+
+    let newBook = {
+        title,
+        author,
+        image,
+        description
+    }
+
+
+    books.push(req.body);
     res.send('Recibido...')
 });
 module.exports = router;
