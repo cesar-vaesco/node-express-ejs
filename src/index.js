46117f8ae1dfb97692f422654a4de303bc135853
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-07-01 12:02:59 -0500

    Creando la estructura de las vistas .ejs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ const app = require('./app');
 
 async function main() {
     await app.listen(app.get('port'))
-    console.log('Server started on port', app.get('port'));
+    console.log('Server started on port', app.get('port'), '--> http://localhost:5000/');
 
 }
-
 
 main();
